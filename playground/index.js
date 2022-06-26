e116ab2a20f12400bb9208a6fcd92de95586c164
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-26 18:16:33 +0200

    WIP

--- playground/index.js
+++ playground/index.js
@@ -1,12 +1,12 @@
 import { BehaviorSubject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 
-const mySubject$ = new BehaviorSubject('Jemand zuhause?');
+const mySubject$ = new ReplaySubject(5);
+
+mySubject$.next('jemand zuhause?');
+mySubject$.next('wirklich?');
 
 mySubject$.subscribe(e => console.log(1, e));
-
-// next() überschreibt den Startwert
-mySubject$.next('ich glaube schon');
-
 mySubject$.subscribe(e => console.log(2, e));
 
 mySubject$.next('na klar');
