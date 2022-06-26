a208eda3a4b80c0dfd60335f85d7bb97d4ce3e6b
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-26 17:57:27 +0200

    WIP

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,15 @@
-import { of, from, timer } from 'rxjs';
+import { filter, map, timer } from 'rxjs';
 
-const obs1$ = of(42, 43, 44);
-const obs2$ = from([1, 2, 3]);
+// const obs1$ = of(42, 43, 44);
+// const obs2$ = from([1, 2, 3]);
 const obs3$ = timer(0, 500); // läuft endlos
 
-obs1$.subscribe(console.log);
-obs2$.subscribe(console.log);
-obs3$.subscribe(console.log);
+// obs1$.subscribe(console.log);
+// obs2$.subscribe(console.log);
 
+obs3$.pipe(
+    filter((x) => x % 2 === 0), // nur gerade
+    map((x) => x * 10)          // ver-10-fachen
+).subscribe(
+    console.log
+);
