6745da7305baae503e3d51087c2ae3e2d33f52df
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-27 19:34:41 +0200

    WIP

---
+++ frontend/books-app/src/app/state/books.state.ts
@@ -0,0 +1,22 @@
+import { Book } from "../models/book";
+
+export interface Books {
+    // Eindeutiges Merkmal (ISBN) als Key
+   [isbn: string]: Book;
+}
+
+export interface Filter {
+  text: string;
+}
+
+export interface BookState {
+  entities: Books;
+  filter: Filter;
+}
+
+export const initialState: BookState = {
+  entities: {},
+  filter: {
+    text: '',
+  },
+};
