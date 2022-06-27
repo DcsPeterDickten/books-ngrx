f98fd7d39f877ebc48f8866429acf32a2bbfe93c
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-27 14:53:30 +0200

    WIP

--- frontend/books-app/src/app/app.component.ts
+++ frontend/books-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
+import { increment, decrement, reset } from './counter/counter.actions';
 
 @Component({
   selector: 'books-root',
@@ -7,11 +9,21 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'books-app';
-
   count$: Observable<number> = of(0);
-  constructor() { }
-  increment() { }
-  decrement() { }
-  reset() { }
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count'); // Selektor
+  }
+
+  increment() {
+    this.store.dispatch(increment()); // Action
+  }
+
+  decrement() {
+    this.store.dispatch(decrement()); // Action
+  }
+
+  reset() {
+    this.store.dispatch(reset()); // Action
+  }
 }
