673c9770ee2e58153291382bb70d9238218a4921
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-28 13:06:22 +0200

    WIP

--- frontend/books-app/src/app/app-routing.module.ts
+++ frontend/books-app/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: BookListComponent },
+  { path: 'create', component: CreateBookComponent },
   { path: 'book/:isbn', component: BookDetailsComponent },
 ];
 
