3ade91c57a86dcd4ec3ba189f5dfd19cf9011016
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-28 16:20:43 +0200

    WIP

--- frontend/books-app/src/app/store/book/book.selectors.ts
+++ frontend/books-app/src/app/store/book/book.selectors.ts
@@ -1,3 +1,18 @@
+import { createSelector } from '@ngrx/store';
 import { RootState } from 'src/app/store/';
 
-export const selectAll = (state: RootState) => Object.values(state.book.entities);
+// anstatt:
+// export const selectAll = (state: RootState) => Object.values(state.book.entities);
+
+// erstmal nur den Teil (Feature/slice) aus dem Gesamt-State holen
+export const selectFeature = (state: RootState) => state.book;
+
+// dann die Bücherliste (entities) extrahieren
+export const selectEntities = createSelector(
+  selectFeature, ({ entities }) => entities
+);
+
+// dann das Objekt zu einem Array wandeln
+export const selectAll = createSelector(
+  selectEntities, (entities) => Object.values(entities)
+);
