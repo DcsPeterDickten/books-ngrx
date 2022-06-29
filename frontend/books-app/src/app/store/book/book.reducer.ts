d4663aeab12a304999ddcee62b52d0b85052e6cf
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-29 16:32:07 +0200

    WIP

--- frontend/books-app/src/app/store/book/book.reducer.ts
+++ frontend/books-app/src/app/store/book/book.reducer.ts
@@ -1,20 +1,15 @@
 import { createReducer, on } from '@ngrx/store';
-import produce from "immer";
 import * as BookActions from './book.actions';
 import { initialState } from "./books.state";
 
 export const bookReducer = createReducer(
   initialState,
-  on(BookActions.neuesBuchGespeichert, (state, { book }) =>
-    produce(state, (draft: any) => {
-
-      draft.entities[book.isbn] = {
-        ...book,
-        available: true,
-      }
-
-    })
-  ),
+  on(BookActions.neuesBuch, (state) => {
+    return {
+      ...state,
+      loading: true, // neu
+    };
+  }),
   on(BookActions.search, (state, { text }) => ({
     ...state,
     filter: {
@@ -24,15 +19,16 @@ export const bookReducer = createReducer(
   })),
   on(BookActions.neuesBuchGespeichert, (state, { book }) => {
     return {
-      ...state,                 // 3 - neuer State
-      entities: {               // 2 - neue Buchliste (entities)
+      ...state,
+      entities: {
         ...state.entities,
-        [book.isbn]: {          // 1 - neues Buch
-          ...book,              // 1
-          available: true,      // 1
+        [book.isbn]: {
+          ...book,
+          available: true,
         },
-      },                        // 2
-    };                          // 3
+      },
+      loading: false // neu
+    };
   })
 );
 
