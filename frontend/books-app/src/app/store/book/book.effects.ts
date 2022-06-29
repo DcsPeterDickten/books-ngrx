ec37d8606d222e6b00eac9403e298bce63b18fb5
Peter Dickten <peter.dickten@dcs-fuerth.de>
2022-06-29 14:44:33 +0200

    WIP

--- frontend/books-app/src/app/store/book/book.effects.ts
+++ frontend/books-app/src/app/store/book/book.effects.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
 import { Actions, createEffect, ofType } from "@ngrx/effects";
+
 import { map, mergeMap } from "rxjs/operators";
 import { Book } from "../../models/book";
 import * as BookActions from "./book.actions";
